3d0044f93e912994ffd9caec92d7d115129cd360
AkashZawar <azawar123@gmail.com>
2020-03-09 22:40:43 +0530

    Base project

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\python projects\django\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myenv/src/pages/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_view(request,*args,**kwargs):
+	print(request)
+	print(args,kwargs)
+	print(request.user)
+	#return HttpResponse(request,"<h1>Hellow world</h1>")
+	my_context = {
+			"pagetitle" : "NightWatch Automation : Home"
+			}
+	return render(request, "index.html", {})
+
+def about_view(request,*args,**kwargs):
+	my_context = {
+			"pagetitle" : "NightWatch Automation : About",
+			"key1" : "value1",
+			"key2" : "value2",
+			"key3" : "value3",
+	}
+	#return HttpResponse("<h1>About world</h1>")
+	return render(request,"about.html",my_context)
+
+def social_view(request,*args,**kwargs):
+	my_context = {
+			"pagetitle" : "NightWatch Automation : Social"
+			}
+	#return HttpResponse("<h1>Social world</h1>")
+	return render(request,"social.html",{})
+
+def contact_view(request,*args,**kwargs):
+	my_context = {
+			"pagetitle" : "NightWatch Automation : Contact"
+			}
+	#return HttpResponse("<h1>Contact world</h1>")
+	return render(request,"contact.html",{})
