56395ec62c97fb5c77865804de234d977f932394
AkashZawar <azawar123@gmail.com>
2020-05-03 19:28:55 +0530

    upload excel file code updated

--- myenv/src/pages/models.py
+++ myenv/src/pages/models.py
@@ -1,27 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Code(models.Model):
-	Title_of_the_Code = models.CharField(max_length=120, default='defaultTitle')
-	Description_of_the_code = models.TextField(blank=True, null=True)
-	# price		=models.DecimalField(decimal_places=2,max_digits=10000)
-	Code = models.TextField(blank=True, null=True)
-	Upload_Code_file = models.FileField(default='null',upload_to='uploads/')
-	uploaded_at = models.DateTimeField(auto_now_add=True)
-	author = models.CharField(max_length=120, default='default_author')
-	language = models.CharField(max_length=120, default='default_Language')
-	vote = models.IntegerField(default=1)
-	score = models.IntegerField(default=1)
-	visits = models.IntegerField(default=1)
+    Title_of_the_Code = models.CharField(max_length=120, default='defaultTitle')
+    Description_of_the_code = models.TextField(blank=True, null=True)
+    # price		=models.DecimalField(decimal_places=2,max_digits=10000)
+    Code = models.TextField(blank=True, null=True)
+    Upload_Code_file = models.FileField(default='null', upload_to='uploads/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=120, default='default_author')
+    language = models.CharField(max_length=120, default='default_Language')
+    vote = models.IntegerField(default=1)
+    score = models.IntegerField(default=1)
+    visits = models.IntegerField(default=1)
 
 
 class Search_page_input(models.Model):
-	searchInput = models.CharField(max_length=120, default='defaultTitle')
-	Title_of_the_Code = models.CharField(max_length=120, default='defaultTitle')
-	value_desc = models.CharField(max_length=120, default='defaultTitle')
-	votes = models.IntegerField(default=0,blank=True)
-	
-class resultForm(models.Model):
-	Functionality_Name = models.CharField(max_length=120)
-	Accuracy = models.IntegerField(default=1)
+    searchInput = models.CharField(max_length=120, default='defaultTitle')
+    Title_of_the_Code = models.CharField(max_length=120, default='defaultTitle')
+    value_desc = models.CharField(max_length=120, default='defaultTitle')
+    votes = models.IntegerField(default=0, blank=True)
+
+
+# class resultForm(models.Model):
+# 	Functionality_Name = models.CharField(max_length=120)
+# 	Accuracy = models.IntegerField(default=1)
+
+
+class CodeInput(models.Model):
+    Test_Action = models.TextField(blank=True, null=False, )
+    locator_type = models.TextField(blank=True, null=True)
+    Test_input_value = models.TextField(blank=True, null=True)
+
+
+class XLUploadModel(models.Model):
+    description = models.CharField(max_length=255, blank=True, default='XLS file name')
+    xl_file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
--- myenv/src/pages/views.py
+++ myenv/src/pages/views.py
@@ -1,70 +1,80 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from .forms import UploadFileForm,SearchForm,resultForm
-from .models import Search_page_input,Code
+from .forms import UploadFileForm, SearchForm, StepInput,XLUpload #resultForm,
+from .models import Search_page_input, Code
 
-def about_view(request,*args,**kwargs):
-	my_context = {
-	"pagetitle" : "NightWatch Automation : About",
-	"key1" : "value1",
-	"key2" : "value2",
-	"key3" : "value3",
-	}
-	#return HttpResponse("<h1>About world</h1>")
-	return render(request,"about.html",my_context)
+
+def about_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = XLUpload(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            # return redirect('about.html')
+            return render(request, 'about.html', {
+                'form': form
+            })
+    else:
+        form = XLUpload()
+    return render(request, 'about.html', {
+        'form': form
+    })
+
+
+# return render(request, "about.html", my_context)
 
 
 def search_view(request, *args, **kwargs):
-	rows = Search_page_input.objects.raw("select id,Title_of_the_Code,Description_of_the_code,vote from pages_code order by vote DESC LIMIT 3")
-	return render(request, "page/search.html",{'rows': rows})
+    rows = Search_page_input.objects.raw(
+        "select id,Title_of_the_Code,Description_of_the_code,vote from pages_code order by vote DESC LIMIT 3")
+    return render(request, "page/search.html", {'rows': rows})
 
 
-def contact_view(request,*args,**kwargs):
-	my_context = {
-	"pagetitle" : "NightWatch Automation : Contact"
-	}
-	#return HttpResponse("<h1>Contact world</h1>")
-	return render(request,"contact.html",{})
+def contact_view(request, *args, **kwargs):
+    my_context = {
+        "pagetitle": "NightWatch Automation : Contact"
+    }
+    # return HttpResponse("<h1>Contact world</h1>")
+    return render(request, "contact.html", {})
 
 
-def social_view(request,*args,**kwargs):
-	my_context = {
-	"pagetitle" : "NightWatch Automation : Contact"
-	}
-	#return HttpResponse("<h1>Contact world</h1>")
-	return render(request,"social.html",{})
+def social_view(request, *args, **kwargs):
+    print(request.GET)
+    context = {'form': StepInput()}
+    return render(request, "social.html", context)
 
 
 # Below code for file upload handling 
 def model_form_upload(request):
-	form = UploadFileForm(request.POST, request.FILES)
-	if form.is_valid():
-		form.save()
-		return redirect('/upload/')
-	else:
-		form = UploadFileForm()
-		return render(request, 'page/model_form_upload.html', {
-		'form': form
-		})
+    form = UploadFileForm(request.POST, request.FILES)
+    if form.is_valid():
+        form.save()
+        return redirect('/upload/')
+    else:
+        form = UploadFileForm()
+        return render(request, 'page/model_form_upload.html', {
+            'form': form
+        })
+    # return render(request, "page/model_form_upload.html", my_context)
 
-	return render(request,"page/model_form_upload.html",my_context)
-# file Upload handling Ends 
+
+# file Upload handling Ends
 
 # Below code for result display 
 def display_result(request):
-	if request.method == 'POST':
-		searchInput = request.POST.get('searchInput')
-		SQL = "select id,Title_of_the_Code,author,language,Code,Upload_Code_file from pages_code where Title_of_the_Code COLLATE UTF8_GENERAL_CI like '%" +searchInput +"%' limit 10"
-		rows = Code.objects.raw(SQL)
-		return render(request, "result.html",{'rows': rows})
-	else:
-		return render(request, "result.html",{})
+    if request.method == 'POST':
+        searchInput = request.POST.get('searchInput')
+        SQL = "select id,Title_of_the_Code,author,language,Code,Upload_Code_file from pages_code where Title_of_the_Code COLLATE UTF8_GENERAL_CI like '%" + searchInput + "%' limit 10"
+        rows = Code.objects.raw(SQL)
+        return render(request, "result.html", {'rows': rows})
+    else:
+        return render(request, "result.html", {})
+
 
 def result_form_submit(request):
-	form = UploadFileForm(request.POST)
-	if form.is_valid():
-		form.save()
-		return redirect('/resultSave/')
-	else:
-		return render(request, "result.html",{})
+    form = UploadFileForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('/resultSave/')
+    else:
+        return render(request, "result.html", {})
